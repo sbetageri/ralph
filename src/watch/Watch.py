d8ebda2204251f42ca04ea71d7860cb6c4b01589
Srirakshith Betageri <sbetageri111@gmail.com>
2019-04-20 19:21:53 -0500

    Change forward pass to return lip features

--- src/watch/Watch.py
+++ src/watch/Watch.py
@@ -1,10 +1,10 @@
 import watch.SyncNetModel as SNet
 
-class Watch:
+class WatchNet:
     def __init__(self, root_dir, model_path):
         '''
 
-        @param root_dir: Root dir of the entire proj
+        @param root_dir: Root dir of the entire project
         @param model_path: Path to the model, relative to the root dir
         '''
         self.root_dir = root_dir
@@ -19,5 +19,11 @@ class Watch:
         @param x: Input tensor
         @return: Features of running input through CNN
         '''
-        x = self.sync_net.forward_lip(x)
+        x = self.sync_net.forward(x)
         return x
+
+    def get_model(self):
+        return self.sync_net
+
+    def get_parameters(self):
+        return self.sync_net.parameters()
