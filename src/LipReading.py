c11255d0484ee6a1d4a2818480bb591cd7d9105f
Srirakshith Betageri <sbetageri111@gmail.com>
2019-04-22 22:29:18 -0500

    Pad text tensors in collate_fn of dataloader

--- src/LipReading.py
+++ src/LipReading.py
@@ -12,6 +12,10 @@ from LRWDataset import LRWDataset
 from torch.utils.data import DataLoader
 import Levenshtein as Lev
 
+SPELL_LAYERS = 3
+SPELL_HIDDEN = 512
+SPELL_OUTPUT = 40
+
 def collate_data_streams(batch):
     mp4_data = []
     mp3_data = []
@@ -24,8 +28,8 @@ def collate_data_streams(batch):
     mp3_pad = pad_sequence(mp3_data, batch_first=True)
 
     mp4_pad = reshape_mp4_tensors(mp4_pad)
-    # txt_pad = pad_sequence(txt_data, batch_first=True)
-    return mp4_pad, mp3_pad, txt_data # _pad, txt_pad
+    txt_pad = pad_sequence(txt_data, batch_first=True, padding_value=38.0)
+    return mp4_pad, mp3_pad, txt_pad# _pad, txt_pad
 
 def reshape_mp4_tensors(mp4):
     b_size, frames, h, w, channels = mp4.size()
@@ -39,10 +43,11 @@ if __name__ == '__main__':
     model_path = 'syncnet_v2.model'
     dataset = LRWDataset(root_dir, dev_dir + 'dev.csv', is_dev=True)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    batch_size = 2
+    batch_size = 8
 
     watch_net = Watch.WatchNet(root_dir, model_path, device)
     listen_net = Listen.ListenNet(device)
+    # spell_net = Spell.SpellNet()
 
     # listen_model = listen.get_model()
     # spell_model = spell.get_model()
@@ -52,9 +57,9 @@ if __name__ == '__main__':
                             batch_size=batch_size,
                             drop_last=True)
 
-    watch_param = watch_net.get_parameters()
-    listen_param = listen_net.get_parameters()
-    # spell_param = spell.get_parameters()
+    watch_param = watch_net.parameters()
+    listen_param = listen_net.parameters()
+    # spell_param = spell_net.get_parameters()
 
     # tot_param = list(watch_param) + list(listen_param) + list(spell_param)
     # optimizer = torch.optim.sgd(tot_param, lr=0.01)
@@ -65,6 +70,7 @@ if __name__ == '__main__':
         ## Move from CPU to GPU, if needed
         mp4 = mp4.to(device)
         mp3 = mp3.to(device)
+        txt = txt.to(device)
 
         video_out, video_states = watch_net.forward(mp4)
         audio_out, audio_states = listen_net.forward(mp3)
@@ -75,5 +81,4 @@ if __name__ == '__main__':
         # spell_out = spell_model(txt, video_out, audio_out, l1_out, l2_out, l3_out)
         # loss = criterion(spell_out, txt)
 
-        assert False
 
